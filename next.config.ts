00dbbd6d8e530686bb52cc483d066f4e20e78572
3NIGMA-ass <incronationgod@gmail.com>
2025-10-13 14:31:06 +0300

    Simplify Next.js config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,8 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 't.me' },
       { protocol: 'https', hostname: 'coin-images.coingecko.com' },
     ],
-    unoptimized: false,
   },
   serverExternalPackages: ['pg'],
-  webpack: (config) => {
-    config.externals.push({
-      'pg-native': 'pg-native',
-    });
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
 };
 
 export default nextConfig;
